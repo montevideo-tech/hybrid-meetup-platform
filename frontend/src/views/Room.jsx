26505f4661009f5bf0db2557c8d24cfac3260fe1
Nicolas Reyes <nicolasreyes@qualabs.com>
2023-02-07 12:18:03 -0300

    WIP: rooms view add dummy participants

--- frontend/src/views/Room.jsx
+++ frontend/src/views/Room.jsx
@@ -1,18 +1,35 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import { React, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 export async function roomLoader({ params }) {
   return params.roomId;
 }
 
 function Room() {
+  const [idCount, setIdCount] = useState(0);
+  const [participants, setParticipants] = useState([]);
   const roomId = useLoaderData();
+  const addParticipant = () => {
+    // adds a dummy participant to test the view's adaptiveness
+    // to the amount of participants in the room
+    const participantsCopy = [...participants];
+    participantsCopy.push({ id: idCount });
+    setIdCount(idCount + 1);
+    setParticipants(participantsCopy);
+  };
   return (
     <div>
-      This will eventually be a room with ID:
-      {' '}
-      {roomId}
+      <h1>
+        Room
+        {' '}
+        {roomId}
+      </h1>
+      {participants.map((participant) => (
+        <div key={participant.id}>dummy</div>
+      ))}
+      <Button onClick={addParticipant}>Add participant</Button>
     </div>
   );
 }
