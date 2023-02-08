1097ee7cc78d498dfde2e9322574a0db9b3b968b
clararfe <clarar@qualabs.com>
2023-02-08 14:46:14 -0300

    fix on api

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export default axios.create({ baseURL: 'https://yyuyncpblcnwlrfpvenq.functions.supabase.co' });
+export default axios.create({ baseURL: process.env.REACT_APP_SUPABASE_FUNCTIONS_URL });
