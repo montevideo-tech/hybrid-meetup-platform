26caba1e22f2c86f77d97b2eb557e9ea0802067a
Felipe De Moura <felipe@mobilerider.com>
2023-02-09 12:12:52 -0500

    added _shared dir by following supabase best practices

--- backend/src/supabase/functions/sign-in-with-google/index.ts
+++ backend/src/supabase/functions/sign-in-with-google/index.ts
@@ -1,10 +1,6 @@
 import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
 import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey',
-}
+import { corsHeaders } from '../_shared/cors.ts'
 
 async function signInWithGoogle(supabaseClient: SupabaseClient) {
   const { data, error } = await supabaseClient.auth.signInWithOAuth({
