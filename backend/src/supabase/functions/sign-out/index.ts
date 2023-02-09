e0ffc0fefc5ba69c848aa1d099fbd058463727b4
Felipe De Moura <felipe@mobilerider.com>
2023-02-09 12:38:50 -0500

    eslint fixed

--- backend/src/supabase/functions/sign-out/index.ts
+++ backend/src/supabase/functions/sign-out/index.ts
@@ -1,45 +1,42 @@
-import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
-import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import { corsHeaders } from '../_shared/cors.ts'
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts';
+import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { corsHeaders } from '../_shared/cors.ts';
 
 async function signOut(supabaseClient: SupabaseClient) {
-    const { data, error } = await supabaseClient.auth.signOut()
-    
-    if(error){
-      return new Response(JSON.stringify({ error }), {
-        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-        status: 400,
-      })
-    }
+  const { data, error } = await supabaseClient.auth.signOut();
 
-    return new Response(JSON.stringify({ data }), {
-        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-        status: 200,
-      })
+  if (error) {
+    return new Response(JSON.stringify({ error }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    });
+  }
+
+  return new Response(JSON.stringify({ data }), {
+    headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+    status: 200,
+  });
 }
 
 serve(async (req) => {
   if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
+    return new Response('ok', { headers: corsHeaders });
   }
 
   try {
-    const supabaseUrl = Deno.env.get('SUPABASE_URL')
-    const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY')
-    const supabaseClient = createClient(
-      supabaseUrl, 
+    const supabaseUrl = Deno.env.get('SUPABASE_URL');
+    const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
+    const supabaseClient = createClient(supabaseUrl,
       supabaseKey,
-      { global: { headers: { Authorization: req.headers.get('User-Authorization')! } } }
-    )
+      { global: { headers: { Authorization: req.headers.get('User-Authorization')! } } });
 
-    return singOut(supabaseClient)
-
+    return singOut(supabaseClient);
   } catch (error) {
-    console.error(error)
+    console.error(error);
 
     return new Response(JSON.stringify({ error }), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 400,
-    })
+    });
   }
-})
+});
--- backend/src/supabase/functions/sign-up/index.ts
+++ backend/src/supabase/functions/sign-up/index.ts
@@ -1,17 +1,17 @@
-import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
-import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import { corsHeaders } from '../_shared/cors.ts'
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts';
+import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { corsHeaders } from '../_shared/cors.ts';
 
 interface User {
-  email: String
-  password: String
-  username: String
+  email: string
+  password: string
+  username: string
   // userId: number
   // role: number
 }
 
 async function signUp(supabaseClient: SupabaseClient, user: User) {
-  const emailExists = await supabaseClient.from('users-data').select('email').eq('email', user.email)
+  const emailExists = await supabaseClient.from('users-data').select('email').eq('email', user.email);
 
   if (emailExists.data && emailExists.data.length !== 0) {
     return new Response(JSON.stringify({
@@ -19,62 +19,59 @@ async function signUp(supabaseClient: SupabaseClient, user: User) {
     }), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 400,
-    })
+    });
   }
-  else {
-    const { data, error } = await supabaseClient.auth.signUp({
-      email: user.email,
-      password: user.password,
-    })
 
-    if (error) {
-      return new Response(JSON.stringify({ error }), {
-        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-        status: 400,
-      })
-    }
+  const { data, error } = await supabaseClient.auth.signUp({
+    email: user.email,
+    password: user.password,
+  });
 
-    // if (user.role == 'host'){
-    //   roleId = 2
-    // }
-
-    await supabaseClient.from('users-data').insert({
-      email: user.email,
-      username: user.username
-      // userId: data.user.id,
-      // roleId: roleId
-    })
-
-    return new Response(JSON.stringify({ data }), {
+  if (error) {
+    return new Response(JSON.stringify({ error }), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-      status: 200,
-    })
+      status: 400,
+    });
   }
 
+  // if (user.role == 'host'){
+  //   roleId = 2
+  // }
+
+  await supabaseClient.from('users-data').insert({
+    email: user.email,
+    username: user.username
+    // userId: data.user.id,
+    // roleId: roleId
+  });
+
+  return new Response(JSON.stringify({ data }), {
+    headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+    status: 200,
+  });
 }
 
 serve(async (req) => {
-  const { url, method } = req
+  const { url, method } = req;
 
   if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
+    return new Response('ok', { headers: corsHeaders });
   }
 
   try {
-    const supabaseUrl = Deno.env.get('SUPABASE_URL')
-    const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY')
-    const supabaseClient = createClient(supabaseUrl, supabaseKey)
+    const supabaseUrl = Deno.env.get('SUPABASE_URL');
+    const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
+    const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
     const body = await req.json();
-    const user = body.user;
-    return signUp(supabaseClient, user)
-
+    const { user } = body;
+    return signUp(supabaseClient, user);
   } catch (error) {
-    console.error(error)
+    console.error(error);
 
     return new Response(JSON.stringify({ error }), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 400,
-    })
+    });
   }
-})
+});
