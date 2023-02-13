ff16cf77d20a1b228ec779ed95b046f27eab9b30
hernan <hernanr@qualabs.com>
2023-02-13 16:40:22 -0300

    Check if the space id is correct

--- backend/src/supabase/functions/room-jwtprovider/index.ts
+++ backend/src/supabase/functions/room-jwtprovider/index.ts
@@ -1,8 +1,18 @@
 import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
+import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import { corsHeaders } from '../_shared/cors.ts';
 import Mux from "https://esm.sh/v106/@mux/mux-node@7.0.0/es2022/mux-node.js";
 
-async function generateToken(spaceId: string) {
+async function generateToken(supabaseClient: SupabaseClient, spaceId: string) {
+  const spacesId = await supabaseClient.from('rooms').select('providerId').eq('providerId', spaceId);
+
+  if(spacesId.data && spacesId.data.length === 0){
+    return new Response(JSON.stringify({ error: 'Wrong Space ID'}), {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 400,
+      });
+  }
+
   let baseOptions = {
     keyId: Deno.env.get('SPACE_KEY_ID'),
     keySecret: Deno.env.get('SPACE_PRIVATE_KEY'),
@@ -20,9 +30,13 @@ serve(async (req) => {
     return new Response('ok', { headers: corsHeaders })
   }
   try {
+    const supabaseUrl = Deno.env.get('SUPABASE_URL');
+    const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
+    const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
     const body = await req.json();
     const spaceId = body.spaceId;
-    return generateToken(spaceId)
+    return generateToken(supabaseClient, spaceId)
   } catch (error) {
     console.error(error);
     return new Response(JSON.stringify({ error }), {
