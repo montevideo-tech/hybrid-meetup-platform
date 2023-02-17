a1e76a41f740e684708fa6b58c69bee4a51ae763
Nicolas Reyes <nicolasreyes@qualabs.com>
2023-02-17 17:10:54 -0300

    edit room view fully functional

--- frontend/src/views/EditRoom.jsx
+++ frontend/src/views/EditRoom.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import {
   List, ListItem, IconButton, Typography, TextField, MenuItem, Button, Card,
@@ -12,11 +12,59 @@ export async function roomLoader({ params }) {
 function EditRoom() {
   const roomId = useLoaderData();
   const [email, setEmail] = useState('');
-  const [role, setRole] = useState('');
-  console.log(role);
+  const [roleToAdd, setRoleToAdd] = useState('host');
+  const [roles, setRoles] = useState({ hosts: [], presenters: [] });
+  const handleAddRole = () => {
+    // TODO post role to server
+    const currentHosts = roles.hosts;
+    const currentPresenters = roles.presenters;
+    if (roleToAdd === 'host') {
+      setRoles({
+        hosts: [...currentHosts, email],
+        presenters: [...currentPresenters],
+      });
+    }
+    if (roleToAdd === 'presenter') {
+      setRoles({
+        hosts: [...currentHosts],
+        presenters: [...currentPresenters, email],
+      });
+    }
+  };
+  const handleDeleteRole = (e, r) => {
+    const currentHosts = roles.hosts;
+    const currentPresenters = roles.presenters;
+    const newRoles = {
+      hosts: [...currentHosts],
+      presenters: [...currentPresenters],
+    };
+    newRoles[r].every((element, index) => {
+      if (element === e) {
+        newRoles[r].splice(index, 1); // remove
+        return false; // break out of loop
+      }
+      return true; // keep looping
+    });
+    setRoles(newRoles);
+  };
+
+  useEffect(() => {
+    // here we load the existing roles (TODO)
+    setRoles({
+      hosts: ['some@email.com', 'other@email.com'],
+      presenters: ['loremipsum@sitamet.com'],
+    }); // hardcoded for testing purposes
+  }, []);
+
   return (
-    <div style={{ padding: '2vh 1vw' }}>
-      <Typography component="h1" variant="h4" sx={{ textAlign: 'left' }}>
+    <div style={{
+      padding: '2vh 1vw',
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+    }}
+    >
+      <Typography component="h1" variant="h4" sx={{ textAlign: 'center' }}>
         Edit Room
         {' '}
         { roomId }
@@ -28,24 +76,23 @@ function EditRoom() {
               Hosts
             </Typography>
             <List>
-              <ListItem
-                secondaryAction={(
-                  <IconButton edge="end" aria-label="????">
-                    <DeleteIcon />
-                  </IconButton>
-                )}
-              >
-                <Typography>Loremipsum@sitamet.com</Typography>
-              </ListItem>
-              <ListItem
-                secondaryAction={(
-                  <IconButton edge="end" aria-label="????">
-                    <DeleteIcon />
-                  </IconButton>
-                )}
-              >
-                <Typography>Loremipsum@sitamet.com</Typography>
-              </ListItem>
+              {
+                /* eslint-disable arrow-body-style */
+                roles.hosts.map((e) => {
+                  return (
+                    <ListItem
+                      key={e}
+                      secondaryAction={(
+                        <IconButton edge="end">
+                          <DeleteIcon onClick={() => { handleDeleteRole(e, 'hosts'); }} />
+                        </IconButton>
+                      )}
+                    >
+                      <Typography>{e}</Typography>
+                    </ListItem>
+                  );
+                })
+              }
             </List>
           </div>
         </Card>
@@ -55,15 +102,23 @@ function EditRoom() {
               Presenters
             </Typography>
             <List>
-              <ListItem
-                secondaryAction={(
-                  <IconButton edge="end" aria-label="????">
-                    <DeleteIcon />
-                  </IconButton>
-                )}
-              >
-                <Typography>Loremipsum@sitamet.com</Typography>
-              </ListItem>
+              {
+                /* eslint-disable arrow-body-style */
+                roles.presenters.map((e) => {
+                  return (
+                    <ListItem
+                      key={e}
+                      secondaryAction={(
+                        <IconButton edge="end">
+                          <DeleteIcon onClick={() => { handleDeleteRole(e, 'presenters'); }} />
+                        </IconButton>
+                      )}
+                    >
+                      <Typography>{e}</Typography>
+                    </ListItem>
+                  );
+                })
+              }
             </List>
           </div>
         </Card>
@@ -87,17 +142,18 @@ function EditRoom() {
               select
               label="Role"
               sx={{ width: 1, marginBottom: '1vh' }}
-              defaultValue="Host"
-              onChange={(e) => setRole(e.target.value)}
+              defaultValue="host"
+              value={roleToAdd}
+              onChange={(e) => setRoleToAdd(e.target.value)}
             >
-              <MenuItem value="Host">
+              <MenuItem value="host">
                 Host
               </MenuItem>
-              <MenuItem value="Presenter">
+              <MenuItem value="presenter">
                 Presenter
               </MenuItem>
             </TextField>
-            <Button variant="contained" onClick={() => { console.log('TODO add role'); }}>
+            <Button variant="contained" onClick={handleAddRole}>
               Add
             </Button>
           </div>
