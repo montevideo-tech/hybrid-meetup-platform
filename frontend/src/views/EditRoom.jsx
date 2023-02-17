ebaafce2cabcc2491a67bde125e3dcefe6428ccc
Nicolas Reyes <nicolasreyes@qualabs.com>
2023-02-17 16:25:56 -0300

    improve styling for edit room view

--- frontend/src/views/EditRoom.jsx
+++ frontend/src/views/EditRoom.jsx
@@ -1,7 +1,7 @@
 import { React, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import {
-  List, ListItem, IconButton, Typography, TextField, MenuItem,
+  List, ListItem, IconButton, Typography, TextField, MenuItem, Button, Card,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -14,76 +14,94 @@ function EditRoom() {
   const [email, setEmail] = useState('');
   const [role, setRole] = useState('');
   console.log(role);
-  const FlexSettings = {
-    display: 'flex',
-  };
   return (
-    <div>
-      Edit Room
-      {' '}
-      { roomId }
-      <div style={FlexSettings}>
-        <div>
-          <Typography>Hosts</Typography>
-          <List>
-            <ListItem
-              secondaryAction={(
-                <IconButton edge="end" aria-label="????">
-                  <DeleteIcon />
-                </IconButton>
-              )}
-            >
-              <Typography>Loremipsum@sitamet.com</Typography>
-            </ListItem>
-            <ListItem
-              secondaryAction={(
-                <IconButton edge="end" aria-label="????">
-                  <DeleteIcon />
-                </IconButton>
-              )}
-            >
-              <Typography>Loremipsum@sitamet.com</Typography>
-            </ListItem>
-          </List>
-        </div>
-        <div>
-          <Typography>Presenters</Typography>
-          <List>
-            <ListItem
-              secondaryAction={(
-                <IconButton edge="end" aria-label="????">
-                  <DeleteIcon />
-                </IconButton>
-              )}
-            >
-              <Typography>Loremipsum@sitamet.com</Typography>
-            </ListItem>
-          </List>
-        </div>
-        <div>
-          <TextField
-            variant="outlined"
-            label="Email"
-            sx={{ width: 1 }}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            select
-            label="Role"
-            sx={{ width: 1 }}
-            defaultValue="Host"
-            onChange={(e) => setRole(e.target.value)}
+    <div style={{ padding: '2vh 1vw' }}>
+      <Typography component="h1" variant="h4" sx={{ textAlign: 'left' }}>
+        Edit Room
+        {' '}
+        { roomId }
+      </Typography>
+      <div style={{ display: 'flex' }}>
+        <Card variant="outlined">
+          <div style={{ padding: '1vh 0.5vw' }}>
+            <Typography component="h2" variant="h5" sx={{ textAlign: 'center' }}>
+              Hosts
+            </Typography>
+            <List>
+              <ListItem
+                secondaryAction={(
+                  <IconButton edge="end" aria-label="????">
+                    <DeleteIcon />
+                  </IconButton>
+                )}
+              >
+                <Typography>Loremipsum@sitamet.com</Typography>
+              </ListItem>
+              <ListItem
+                secondaryAction={(
+                  <IconButton edge="end" aria-label="????">
+                    <DeleteIcon />
+                  </IconButton>
+                )}
+              >
+                <Typography>Loremipsum@sitamet.com</Typography>
+              </ListItem>
+            </List>
+          </div>
+        </Card>
+        <Card sx={{ marginLeft: '2vw' }} variant="outlined">
+          <div style={{ padding: '1vh 0.5vw' }}>
+            <Typography component="h2" variant="h5" sx={{ textAlign: 'center' }}>
+              Presenters
+            </Typography>
+            <List>
+              <ListItem
+                secondaryAction={(
+                  <IconButton edge="end" aria-label="????">
+                    <DeleteIcon />
+                  </IconButton>
+                )}
+              >
+                <Typography>Loremipsum@sitamet.com</Typography>
+              </ListItem>
+            </List>
+          </div>
+        </Card>
+        <Card sx={{ marginLeft: '2vw' }} variant="outlined">
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyItems: 'center',
+            padding: '1vh 0.5vw',
+          }}
           >
-            <MenuItem value="Host">
-              Host
-            </MenuItem>
-            <MenuItem value="Presenter">
-              Presenter
-            </MenuItem>
-          </TextField>
-        </div>
+            <TextField
+              variant="outlined"
+              label="Email"
+              sx={{ width: 1, marginBottom: '1vh' }}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              select
+              label="Role"
+              sx={{ width: 1, marginBottom: '1vh' }}
+              defaultValue="Host"
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <MenuItem value="Host">
+                Host
+              </MenuItem>
+              <MenuItem value="Presenter">
+                Presenter
+              </MenuItem>
+            </TextField>
+            <Button variant="contained" onClick={() => { console.log('TODO add role'); }}>
+              Add
+            </Button>
+          </div>
+        </Card>
       </div>
     </div>
   );
