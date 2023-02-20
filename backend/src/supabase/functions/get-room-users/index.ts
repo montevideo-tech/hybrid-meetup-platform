82cace980e07b1d6d0848b0b805a9cdebab05413
clararfe <clarar@qualabs.com>
2023-02-20 16:58:37 -0300

    edge function added

---
+++ backend/src/supabase/functions/get-room-users/index.ts
@@ -0,0 +1,61 @@
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { corsHeaders } from '../_shared/cors.ts';
+
+async function returnError(msgError: string) {
+  return new Response(JSON.stringify({
+    error: msgError
+  }), {
+    headers: {
+      ...corsHeaders,
+      'Content-Type': 'application/json'
+    },
+    status: 400
+  });
+}
+
+async function getUsers(supabaseClient, body) {
+  let { providerId } = body;
+
+  const roomData = await supabaseClient.from('rooms-data').select('permissionId, userId').eq('providerId', providerId);
+  if (!roomData.data || (roomData.data?.length === 0))
+    return returnError("No room with given data exists");
+
+  return new Response(JSON.stringify({
+    roomData: roomData.data,
+  }), {
+    headers: {
+      ...corsHeaders,
+      'Content-Type': 'application/json'
+    },
+    status: 200
+  });
+
+}
+
+serve(async (req) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', {
+      headers: corsHeaders,
+    });
+  }
+
+  try {
+    const supabaseUrl = Deno.env.get('SUPABASE_URL');
+    const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
+    const supabaseClient = createClient(supabaseUrl, supabaseKey);
+    const body = await req.json();
+
+    return getUsers(supabaseClient, body);
+  } catch (error) {
+    return new Response(JSON.stringify({
+      error,
+    }), {
+      headers: {
+        ...corsHeaders,
+        'Content-Type': 'application/json'
+      },
+      status: 500,
+    });
+  }
+});
