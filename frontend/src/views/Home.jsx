6482e29c93f8a26f910ae4714777be0648c16275
Gaston Bogarin <gastonb@qualabs.com>
2023-02-23 09:09:30 -0300

    Data persistance in store using redux-persist

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import { Navigate } from 'react-router-dom';
 import React from 'react';
-import store from '../store';
+import { store } from '../store';
 
 function Home() {
   return (
-    store.getState().loggedUser.email ? <Navigate to="/rooms" /> : <Navigate to="/signIn" />
+    store.getState().user.email ? <Navigate to="/rooms" /> : <Navigate to="/signIn" />
   );
 }
 
