96e622e450f512a449092bcaf62ad2f5871d9d20
yulianag <yulianag@qualabs.com>
2023-05-03 15:07:19 -0300

    192: Refactor code in RedirectLoggedInUser and RequireAuth to avoid duplication

---
+++ frontend/src/utils/get-user-token.js
@@ -0,0 +1,8 @@
+import { store } from '../store';
+
+export const getUserToken = () => {
+  const userToken = store.getState().user?.token;
+  return userToken || null;
+};
+
+export default getUserToken;
