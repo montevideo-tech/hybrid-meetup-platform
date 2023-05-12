0d48aad8a518770dc8a5e737374fedf6299425dd
paoopintos <paolap@qualabs.com>
2023-05-12 16:08:40 -0300

    remove Home component because it's the Landing Page now

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ThemeProvider } from '@mui/material';
-import theme from './themes/theme';
-import './index.css';
-import { store } from './store';
-import IsAdmin from './components/IsAdmin';
-import Root from './views/Root';
-import SignIn from './views/SignIn';
-import SignUp from './views/SignUp';
-import Room, { roomLoader } from './views/Room';
-import Rooms from './views/Rooms';
-import ErrorPage from './views/ErrorPage';
-import Home from './views/Home';
-import RoomNotFound from './views/RoomNotFound';
-import EditRoom from './views/EditRoom';
-import { RoomsLayout } from './layout/RoomsLayout';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material";
+import theme from "./themes/theme";
+import "./index.css";
+import { store } from "./store";
+import IsAdmin from "./components/IsAdmin";
+import Root from "./views/Root";
+import SignIn from "./views/SignIn";
+import SignUp from "./views/SignUp";
+import Room, { roomLoader } from "./views/Room";
+import Rooms from "./views/Rooms";
+import ErrorPage from "./views/ErrorPage";
+import RoomNotFound from "./views/RoomNotFound";
+import EditRoom from "./views/EditRoom";
+import { RoomsLayout } from "./layout/RoomsLayout";
 
 // import RoomTest from './components/RoomTest';
-import AuthRoute from './components/AuthRoute';
+import AuthRoute from "./components/AuthRoute";
+import LandingPage from "./views/LandingPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '',
-        element: <Home />,
+        path: "",
+        element: <LandingPage />,
       },
       {
-        path: '/rooms',
+        path: "/rooms",
         element: (
           <AuthRoute requireAuth>
             <RoomsLayout />
@@ -40,22 +40,20 @@ const router = createBrowserRouter([
         ),
         children: [
           {
-            path: '',
+            path: "",
             element: <Rooms />,
           },
           {
-            path: '404',
+            path: "404",
             element: <RoomNotFound />,
           },
           {
-            path: ':roomId',
-            element: (
-              <Room />
-            ),
+            path: ":roomId",
+            element: <Room />,
             loader: roomLoader,
           },
           {
-            path: ':roomId/edit',
+            path: ":roomId/edit",
             element: (
               <IsAdmin>
                 <EditRoom />
@@ -63,10 +61,10 @@ const router = createBrowserRouter([
             ),
             loader: roomLoader,
           },
-        ]
+        ],
       },
       {
-        path: '/signIn',
+        path: "/signIn",
         element: (
           <AuthRoute>
             <SignIn />
@@ -74,7 +72,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: '/signUp',
+        path: "/signUp",
         element: (
           <AuthRoute>
             <SignUp />
@@ -93,9 +91,11 @@ const router = createBrowserRouter([
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    <Provider store={store}><RouterProvider router={router} /></Provider>
-  </ThemeProvider>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </ThemeProvider>,
 );
