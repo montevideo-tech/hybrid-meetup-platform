d990c9c74037b0ce5ad09e98485c6ad6fdcb6c77
Juan Piquerez <juanpa.piquerez@hotmail.com>
2023-06-28 16:29:41 -0300

    env variable for development mode and production mode

---
+++ frontend/src/utils/environment.js
@@ -0,0 +1,26 @@
+import { supabase } from "../lib/api";
+import { VITE_WEBRTC_PROVIDER_NAME, VITE_DOLBY_API_KEY } from "../lib/constants";
+
+const getProvider = async () => {
+  if (VITE_WEBRTC_PROVIDER_NAME) {
+    return VITE_WEBRTC_PROVIDER_NAME;
+  }
+  const { data } = await supabase
+    .from('environment')
+    .select('value')
+    .eq('key', 'PROVIDER_NAME');
+  return data[0]?.value;
+}
+
+const getDolbyKey = async () => {
+  if (VITE_DOLBY_API_KEY) {
+    return VITE_DOLBY_API_KEY;
+  }
+  const { data } = await supabase
+    .from('environment')
+    .select('value')
+    .eq('key', 'DOLBY_API_KEY');
+  return data[0]?.value;
+}
+
+export {getProvider, getDolbyKey}
